1b7f4ffe1b3d27a3ab78e644a327af6aeeb6af11
unknown <toufeeqk14>
2026-04-14 18:29:09 +0530

    first commit

---
+++ src/main/java/com/alamin/alamin_bank/AlaminBankApplication.java
@@ -0,0 +1,13 @@
+package com.alamin.alamin_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaminBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaminBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alamin/alamin_bank/AlaminBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.alamin_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaminBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
